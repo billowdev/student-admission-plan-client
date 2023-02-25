8f7965c146257a616566bfdbca0d63a5326fecd6
pipatpong1412 <tfghgh.fg@gmail.com>
2023-02-25 22:51:58 +0700

    Create Drawer

--- sapms_app/lib/pages/main_menu/widgets/course_information_button_widget.dart
+++ sapms_app/lib/pages/main_menu/widgets/course_information_button_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project/course_information.dart';
+import 'package:project/pages/main_menu/course_information.dart';
 
 class CourseInformationButtonWidget extends StatelessWidget {
   const CourseInformationButtonWidget({Key? key}) : super(key: key);
@@ -11,18 +11,21 @@ class CourseInformationButtonWidget extends StatelessWidget {
       width: double.infinity,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.white,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(50),
-                  side: const BorderSide(color: Colors.green, width: 1)),
-            ),
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50),
+              side: const BorderSide(color: Colors.green, width: 1)),
+        ),
         onPressed: () {
-          Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => const CourseInformation()));
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => const CourseInformation()));
         },
         child: const Text(
           'ข้อมูลหลักสูตร',
-          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,color: Colors.green),
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.green),
           textAlign: TextAlign.center,
         ),
       ),
--- sapms_app/test/widget_test.dart
+++ sapms_app/test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:project/main.dart';
+import 'package:project/pages/main_menu/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
