c191ab6045b05c10c8b11dea4d764c9cc31557b6
Akkarapon Phikulsri <akkarapon@billowdev.com>
2023-03-05 13:39:17 +0700

    course and login page

---
+++ lib/pages/admission_plan/admission_plan.page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AdmissionPlanPage extends StatefulWidget {
+  const AdmissionPlanPage({super.key});
+
+  @override
+  State<AdmissionPlanPage> createState() => _AdmissionPlanPageState();
+}
+
+class _AdmissionPlanPageState extends State<AdmissionPlanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/pages/course/course_detail_page.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:project/pages/course/models/course.model.dart';
+import '../../common/widgets/appbar.widget.dart';
+import '../../common/widgets/drawer.widget.dart';
+import 'edit_course_page.dart';
+
+class CourseDetailPage extends StatefulWidget {
+  final CoursePayload detail;
+  const CourseDetailPage({super.key, required this.detail});
+  @override
+  State<CourseDetailPage> createState() => _CourseDetailPageState();
+}
+
+class _CourseDetailPageState extends State<CourseDetailPage> {
+  late String _major;
+  late String _degree;
+  late String _faculty;
+  late String _qualification;
+  static String apiUrl = dotenv.env['API_URL'].toString();
+
+  @override
+  void initState() {
+    super.initState();
+    _major = widget.detail.major!;
+    _degree = widget.detail.degree!;
+    _faculty = widget.detail.faculty!;
+    _qualification = widget.detail.qualification!;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      appBar: const AppBarWidget(txtTitle: 'รายละเอียดข้อมูลหลักสูตร'),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: RichText(
+                  text: const TextSpan(
+                text: 'สาขาวิทยาการคอมพิวเตอร์',
+                style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.black,
+                    fontFamily: 'PrintAble4U',
+                    fontWeight: FontWeight.bold),
+              )),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Table(
+                children: [
+                  _buildTableRow('ชื่อหลักสูตร', _major.toString()),
+                  _buildTableRow('หลักสูตร', _degree.toString()),
+                  _buildTableRow('คณะ', _faculty.toString()),
+                  _buildTableRow('รายละเอียด', _qualification.toString()),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      drawer: const DrawerMenuWidget(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => EditCourseDetailPage(detail: widget.detail),
+            ),
+          );
+        },
+        child: Icon(Icons.edit),
+      ),
+    ));
+  }
+
+  TableRow _buildTableRow(String title, String content) {
+    return TableRow(children: [
+      TableCell(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+          child: Text(
+            title,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+        ),
+      ),
+      TableCell(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 8.0),
+          child: Text(content),
+        ),
+      ),
+    ]);
+  }
+}
